783874e297ec365d74a59ce31edde9979785ac53
mishra-kuldeep <kuldeep02071991@gmail.com>
2021-02-25 23:27:12 +0530

    added changes

---
+++ js/common.js
@@ -0,0 +1,61 @@
+//main navigation
+
+jQuery(document).ready(function($){
+
+
+// Navbar
+$( "<span class='clickD'></span>" ).insertAfter(".navbar-nav li.menu-item-has-children > a");
+ $('.navbar-nav li .clickD').click(function(e) {
+    e.preventDefault();
+    var $this = $(this);
+  
+    if ($this.next().hasClass('show')) {
+        $this.next().removeClass('show');
+        $this.removeClass('toggled');
+    } else {
+        $this.parent().parent().find('.sub-menu').removeClass('show');
+        $this.parent().parent().find('.toggled').removeClass('toggled');
+        $this.next().toggleClass('show');
+        $this.toggleClass('toggled');
+    }
+});
+
+$(window).on('resize', function(){
+    var win = $(this); //this = window
+    if (win.width() < 1025) {
+        $('html').click(function(){
+            $('.navbar-nav li .clickD').removeClass('toggled');
+            $('.toggled').removeClass('toggled');
+            $('.sub-menu').removeClass('show');
+        });
+        $(document).click(function(){
+            $('.navbar-nav li .clickD').removeClass('toggled');
+            $('.toggled').removeClass('toggled');
+            $('.sub-menu').removeClass('show');
+        });
+        $('.navbar-nav').click(function(e){
+        e.stopPropagation();
+        });
+     }
+});
+// Navbar end
+
+
+
+
+// $('.banner_sec').slick({
+//   dots: true,
+//   infinite: true,
+//   arrows: false,
+//   speed: 300,
+//   slidesToShow: 1,
+//   adaptiveHeight: true
+// });
+
+
+
+
+
+})
+
+
